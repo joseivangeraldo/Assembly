90abe822d401a391cf4e496e75af054d87fb1d5f
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2025-01-06 05:40:33 +0000

    2025-01-06 05:40:30 ' Acrescentado'

--- NaPratica/maiornum/maiornum.asm
+++ NaPratica/maiornum/maiornum.asm
@@ -13,15 +13,35 @@ segment .data
     STD_OUT     equ 0x1 ;;Saida padrao
 
 section .data
-    x   dd  50  ;;db dw, dd define double word
-    y   dd  10
-    msg1 db  'x maior que y, LF, NULL
+    x   dd  10  ;;db dw, dd define double word
+    y   dd  50
+    msg1 db  'X maior que Y', LF, NULL
     tam1    equ $ - msg1
-    msg2 db  'Y maior que X, LF, NULL
+    msg2 db  'Y maior que X', LF, NULL
     tam2    equ $ - msg2
 
 section .text
-    global_start
+    global _start
         _start:
-            mov EAX, DWORD[x]
-            mov EAX, DWORD[y]
+            mov EAX, DWORD[x] 
+            mov EBX, DWORD[y]
+;;if : comparação
+        cmp EAX, EBX
+        ; je= jg> jge>= jl< jle<= jne!=  salto condicional
+        jge maior   ;;EAX >= EBX
+        mov ECX, msg2
+        mov EDX, tam2
+        jmp final
+        ;jmp goto salto incondicional
+maior:
+    mov ECX, msg1
+    mov EDX, tam1
+
+final:
+    mov EAX, SYS_WRITE
+    mov EBX, STD_OUT
+    int SYS_CALL
+
+    mov EAX, SYS_EXIT
+    mov EBX, RET_EXIT
+    int SYS_CALL
