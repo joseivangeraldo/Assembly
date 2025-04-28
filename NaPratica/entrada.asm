48c2c869e2318e8d32d216b2e199a6b3cb03c7af
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2025-04-28 08:08:59 +0000

    2025-04-28 08:08:56 ' Acrescentado'

--- NaPratica/entrada.asm
+++ NaPratica/entrada.asm
@@ -1,43 +1,44 @@
 segment .data
-    LF          equ 0xA ;;Line feed
+    LF          equ 0xA ;;Line Feed ,final de STRING
     NULL        equ 0xD  ;;Final da String NULO
-    SYS_CALL    equ 0x80    ;;
+    SYS_CALL    equ 0x80  ;;envia informação ao SO
     
     ;   EAX
-    SYS_EXIT    equ 0x1 ;;Codigo de chamadapara finalizar      
+    SYS_EXIT    equ 0x1 ;;Codigo de chamada para finalizar programa     
     SYS_READ    equ 0x3 ;;operação de leitura
-    SYS_WRITE   equ 0x4 ;;operação deescrita
+    SYS_WRITE   equ 0x4 ;;operação de escrita
+
     ;   EBX
     RET_EXIT    equ 0x0 ;;operação realizada com sucesso
     STD_IN      equ 0x0 ;;entrada padrão 
     STD_OUT     equ 0x1 ;;Saida padrao
 
 
-section .data
+section .data  ;;É SÓ PARA TERMOS CONSTANTES
     msg db  "Entre com seu nome:", LF , NULL
     tam equ $ - msg
 
-section .bss   ;;;aqui coloca as variaveis
-    nome    resb    1   ;;;recebe um byte e armazena de 1 em 1
+section .bss   ;;;AQUI COLOCA AS VARIAVEIS
+    nome    resb    1   ;;UMA VARIAVEL DE BYTE e VAI ARMAZENANDO DE 1 EM 1.
 
 section .text
 
 global _start
 
     _start:
-        mov EAX,    SYS_WRITE
-        mov EBX,    STD_OUT
-        mov ECX,    msg
-        mov EDX,    tam
-        int SYS_CALL
+        mov EAX,    SYS_WRITE ;;ESCREVE
+        mov EBX,    STD_OUT ;;LINHA DE SAIDA
+        mov ECX,    msg ;;MENSAGEM
+        mov EDX,    tam ;;TAMANHO DA MENSAGEM
+        int SYS_CALL  ;;SO SE ENCARREGA DE MANDAR A INFORMAÇÃO
 
-        mov EAX,    SYS_READ
-        mov EBX,    STD_IN
-        mov ECX,    nome
+        mov EAX,    SYS_READ ;;03
+        mov EBX,    STD_IN ;;DIZER QUE É UMA ENTRADA
+        mov ECX,    nome ;;NOSSO NOME
         mov EDX,    0xA ;;limitamos o tamanho para 10
-        int SYS_CALL
+        int SYS_CALL  ;;  SAIDA SO
 
-        mov EAX,    SYS_EXIT
-        mov EBX,    RET_EXIT
-        int SYS_CALL
+        mov EAX,    SYS_EXIT ;;
+        mov EBX,    RET_EXIT  ;;
+        int SYS_CALL ;;
 
