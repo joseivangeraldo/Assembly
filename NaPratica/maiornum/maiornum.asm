23cf0325c2ea2f5904788c210878283db537aff1
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2025-04-29 07:10:59 +0000

    2025-04-29 07:10:56 ' Acrescentado'

--- NaPratica/maiornum/maiornum.asm
+++ NaPratica/maiornum/maiornum.asm
@@ -15,17 +15,19 @@ segment .data
 section .data
     x   dd  10  ;;db dw, dd define double word
     y   dd  50
+
     msg1 db  'X maior que Y', LF, NULL
     tam1    equ $ - msg1
+
     msg2 db  'Y maior que X', LF, NULL
     tam2    equ $ - msg2
 
 section .text
     global _start
         _start:
-            mov EAX, DWORD[x] 
+            mov EAX, DWORD[x]
             mov EBX, DWORD[y]
-;;if : comparação
+;;if . comparação
         cmp EAX, EBX
         ; je= jg> jge>= jl< jle<= jne!=  salto condicional
         jge maior   ;;EAX >= EBX
@@ -43,5 +45,11 @@ final:
     int SYS_CALL
 
     mov EAX, SYS_EXIT
-    mov EBX, RET_EXIT
+    mov EBX, RET_EXIT  ;;ALTERNATIVO xor EBX, EBX
     int SYS_CALL
+
+;        AND         OR          xor
+;7 -     0111        0111        0111
+;5 -     0101        0101        0101
+;        0101        0111        0010
+;           5           7           2               
