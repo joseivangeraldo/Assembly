db2b881cb6ece477d60599f30ec64b1e7a359aa0
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2025-05-04 20:22:22 +0000

    2025-05-04 20:22:19 ' Acrescentado'

--- NaPratica/Biblioteca/biblioteca.asm
+++ NaPratica/Biblioteca/biblioteca.asm
@@ -5,13 +5,25 @@ section .text
 global _start
 
 _start:
-    lea esi,    [BUFFER] ;;load effective address
-    address esi,    0x9
+    lea esi, [BUFFER] ;;
+    ;;load effective address (lea) 
+    ;;Tem o poder de associar um endereço do bss 
+    ;;com um registrador aqui.
+    ;;TUDO QUE ESTOU MANDANDO PARA A ESI ESTOU MANDANDO PARA O BUFFER
+    ;;CUIDADO NÃO PODE MANDAR REGISTRADOR PARA A ESI SE NÃO PERDE A CONEXÃO
+    
+    add esi,    0x9 ;;  NA TABELA HEXADECIAML É UM COMEÇO DE UMA STRING
     mov byte[esi],  0xA
 
-    ;;'DCBA/n'
+    ;;'DCBA/n' ENTRA NO REGISTRADOR DE TRAS PARA FRENTE
 
-    dec esi ;;MOVE ESI para ficar na ordem
+    dec esi ;;MOVE ESI para TRAS
     mov dl, 0x11 ;; 11 é um numero 'A' OX11   --> 0X31 'a'
     add dl, '0' ;;CONVERTE O CARACTER EM UM NUMERO
     mov [esi],  dl
+
+    call saidaResultado
+
+    mov eax, SYS_EXIT
+    mov ebx, RET_EXIT
+    int SYS_EXIT
